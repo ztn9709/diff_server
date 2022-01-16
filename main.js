fd4e21c42ff63796f80daedd6e7aa9c02ef93733
ztn_home <zhutiannian15@mails.ucas.ac.cn>
2022-01-16 21:32:13 +0800

    MDB-20220116-1

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 
 //引入路由容器
-let router = require('./router')
+const router = require('./router')
 
-let server = express()
+const server = express()
 
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: true }))
+server.use(express.json())
+server.use(express.urlencoded({ extended: false }))
 server.use(cors())
 
 server.use('/public/', express.static('./public/'))
@@ -17,4 +16,4 @@ server.use('/node_modules/', express.static('./node_modules/'))
 //挂载路由容器到服务中
 server.use(router)
 
-server.listen(4000)
+server.listen(4001)
--- router.js
+++ router.js
@@ -1,12 +1,104 @@
 const express = require('express')
+const thenFs = require('then-fs')
+const formidable = require('formidable')
 const fs = require('fs')
 
 //创建路由容器
 let router = express.Router()
 
-// router.get('/api/classes', async function (req, res) {
-//   const classes_data = await Class.find()
-//   res.send(classes_data)
+router.post('/api/search', (req, res) => {
+  const form = new formidable.IncomingForm()
+  form.parse(req, async (err, fields, files) => {
+    if (fields.formula === 'Co3Sn2S2') {
+      const data = await thenFs.readFile('./newData.json', 'utf8')
+      res.send(data)
+    } else {
+      res.status(404).send('404 Not Found')
+    }
+  })
+})
+router.get('/api/material', async (req, res) => {
+  if (req.query.formula === 'Co3Sn2S2') {
+    const data = await thenFs.readFile('./newData.json', 'utf8')
+    res.send(data)
+  } else {
+    res.status(404).send('404 Not Found')
+  }
+})
+// router.get('/api/search', async (req, res) => {
+//   const data = JSON.parse(await thenFs.readFile('./CoFeSnS.json', 'utf8'))
+//   data[0].transport_data.forEach(item => {
+//     item.M_T_data.data = item.M_T_data.data
+//       .trim()
+//       .split(' ')
+//       .map((val, index, arr) => {
+//         if (index % 2 == 0) {
+//           return [parseFloat(val), parseFloat(arr[index + 1])]
+//         }
+//       })
+//       .filter(val => val)
+//   })
+//   data[0].transport_data.forEach(item => {
+//     item.Rxx_T_data.data = item.Rxx_T_data.data
+//       .trim()
+//       .split(' ')
+//       .map((val, index, arr) => {
+//         if (index % 2 == 0) {
+//           return [parseFloat(val), parseFloat(arr[index + 1])]
+//         }
+//       })
+//       .filter(val => val)
+//   })
+//   data[0].transport_data.forEach(item => {
+//     item.AHC_T_data.data = item.AHC_T_data.data
+//       .trim()
+//       .split(' ')
+//       .map((val, index, arr) => {
+//         if (index % 2 == 0) {
+//           return [parseFloat(val), parseFloat(arr[index + 1])]
+//         }
+//       })
+//       .filter(val => val)
+//   })
+//   data[0].transport_data.forEach(item => {
+//     item.AHA_T_data.data = item.AHA_T_data.data
+//       .trim()
+//       .split(' ')
+//       .map((val, index, arr) => {
+//         if (index % 2 == 0) {
+//           return [parseFloat(val), parseFloat(arr[index + 1])]
+//         }
+//       })
+//       .filter(val => val)
+//   })
+//   data[0].transport_data.forEach(item => {
+//     item.M_B_data.data = item.M_B_data.data
+//       .trim()
+//       .split(' ')
+//       .map((val, index, arr) => {
+//         if (index % 2 == 0) {
+//           return [parseFloat(val), parseFloat(arr[index + 1])]
+//         }
+//       })
+//       .filter(val => val)
+//   })
+//   data[0].transport_data.forEach(item => {
+//     item.Rxy_B_data.data = item.Rxy_B_data.data
+//       .trim()
+//       .split(' ')
+//       .map((val, index, arr) => {
+//         if (index % 2 == 0) {
+//           return [parseFloat(val), parseFloat(arr[index + 1])]
+//         }
+//       })
+//       .filter(val => val)
+//   })
+//   fs.writeFile('newData.json', JSON.stringify(data), err => {
+//     if (err) {
+//       console.log('写入失败')
+//     }
+//   })
+//   res.send('ok')
 // })
 
 // router.get('/api/paper', async function (req, res) {
