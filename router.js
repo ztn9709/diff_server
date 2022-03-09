42ad4866313902232361a3e6d2d82da6ec858526
ztn <zhutiannian15@mails.ucas.ac.cn>
2022-03-09 15:53:53 +0800

    NLODB-220309

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const formidable = require('formidable')
+const thenfs = require('then-fs')
 const Material = require('./mongoose.js')
 
 //创建路由容器
@@ -33,7 +34,20 @@ router.post('/api/material', (req, res) => {
 })
 router.get('/api/material', async (req, res) => {
   try {
-    const data = await Material.findOne({ id: req.query.id })
+    let data = await Material.findOne({ id: req.query.id })
+    data = JSON.parse(JSON.stringify(data))
+    let src1 = __dirname + '/public/static/data/bandplot/bandplot_nsoc/' + data.mp_id + '.svg'
+    let src2 = __dirname + '/public/static/data/bandplot/bandplot_soc/' + data.mp_id + '.svg'
+    try {
+      data.bandplot_nsoc = await thenfs.readFile(src1, 'utf-8')
+    } catch (err) {
+      data.bandplot_nsoc = null
+    }
+    try {
+      data.bandplot_soc = await thenfs.readFile(src2, 'utf-8')
+    } catch (err) {
+      data.bandplot_soc = null
+    }
     res.send(data)
   } catch (err) {
     res.status(500).send('数据库查询出错:' + err)
